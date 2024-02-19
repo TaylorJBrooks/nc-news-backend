4154bea3a604fca11b6d03c7c94bab63b6cb27a2
TaylorJBrooks <taylorjanebrooks@gmail.com>
2024-02-19 14:21:42 +0000

    GET /api/articles/:article_id endpoint completed and tested

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const { getTopics } = require('./controllers/topics-controllers');
-const { pathNotFound } = require('./error-handling');
+const { pathNotFound, customError, badRequest } = require('./error-handling');
 const { getApi } = require('./controllers/api-controllers');
+const { getArticleById } = require('./controllers/articles-controllers');
 const app = express();
 
 app.get('/api', getApi);
 app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticleById);
 
 app.all('/*', pathNotFound);
+app.use(customError);
+app.use(badRequest);
 
 module.exports = app;
--- error-handling.js
+++ error-handling.js
@@ -1,3 +1,17 @@
 exports.pathNotFound = (req, res, next) => {
     res.status(404).send({msg: "path not found"});
 };
+
+exports.customError = (err, req, res, next) => {
+    if(err.status && err.msg){
+        res.status(err.status).send({msg: err.msg});
+    }
+    else next(err);
+};
+
+exports.badRequest = (err, req, res, next) => {
+    if(err.code === '22P02'){
+        res.status(400).send({msg: 'bad request'});
+    }
+    else next(err);
+}
