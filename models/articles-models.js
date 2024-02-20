d92bf01c6eaf0d13606af4eff954e05c2679e26b
TaylorJBrooks <taylorjanebrooks@gmail.com>
2024-02-20 16:46:24 +0000

    Feature Request Complete: GET /api/articles/:article_id returned object contains comment_count property

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,8 +1,16 @@
 const db = require("../db/connection");
 
 exports.selectArticleById = (article_id) => {
+    const queryString = `
+    SELECT articles.*, 
+    COUNT(comment_id) ::INT AS comment_count 
+    FROM articles 
+    LEFT JOIN comments ON articles.article_id = comments.article_id 
+    WHERE articles.article_id = $1 
+    GROUP BY articles.article_id`;
+    
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .query(queryString, [article_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({
